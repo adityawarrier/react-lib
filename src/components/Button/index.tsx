54f987f7d1c1bc5037626f7f7f895ee1678a9180
Aditya Warrier <aditya.warrier.unni2@gmail.com>
2022-02-22 22:35:46 +0530

    Minor changes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,9 +6,17 @@ export interface IButtonProps {
 
 export const Button = (props: IButtonProps) => {
   const { title, onClick } = props;
+  console.log(onClick);
+
   return (
-    <button className={classes.container} onClick={onClick}>
-      {title}
+    <button
+      className={classes.container}
+      onClick={() => {
+        console.log("onClick called!");
+        onClick();
+      }}
+    >
+      {title.toUpperCase()}
     </button>
   );
 };
